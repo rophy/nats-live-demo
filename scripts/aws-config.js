2523f7f734cc147bbbc03df85faf1519e292a790
Rophy Tsai <rophy@users.noreply.github.com>
2022-05-14 16:04:07 +0800

    initial commit with Messaging.zip downloaded from: https://workshops.devax.academy/monoliths-to-microservices/module5/explore_messaging_options/setup-s3-bucket-website.html

---
+++ scripts/aws-config.js
@@ -0,0 +1,38 @@
+var config = {
+
+	"Region" : "ap-southeast-2",
+
+	"Cognito" : {
+		"IdentityPoolId": "REPLACE_WITH_COGNITO_IDENTITY_POOL_ID"
+	},
+
+	"SQS" : {
+		"Standard" : {
+			"QueueUrl" : "REPLACE_WITH_QUEUE_URL_1"
+		},
+		"Secondary" : {
+			"QueueUrl" : "REPLACE_WITH_QUEUE_URL_2"
+		},
+		"Tertiary" : {
+			"QueueUrl" : "REPLACE_WITH_QUEUE_URL_3"
+		},
+		"FIFO" : {
+			"QueueUrl" : "REPLACE_WITH_QUEUE_URL_FIFO",
+			"GroupId" : "idevelop_sqs_fifo"
+		}
+	},
+
+	"SNS" : {
+		"TopicARN" : "REPLACE_WITH_SNS_TOPIC_ARN"
+	},
+
+	"Kinesis" : {
+		"StreamName" 		: "iDevelopDrawingData",
+		"PartitionKey"	: "Partition1"
+	},
+
+	"IoT" : {
+		"Endpoint" : "REPLACE_WITH_IOT_ENDPOINT",
+		"Topic" : "idevelop/drawingdemo"
+	}
+}
